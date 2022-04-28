caac7b0240beb9b261da293ed620984402de2cc2
Ujjwal Gupta <52441813+ujjwal18bit1167@users.noreply.github.com>
2022-04-29 00:04:08 +0530

    Delete src directory

--- src/main/java/com/ecommerce/major/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.major.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ecommerce.major.model.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Integer>{
-
-}
