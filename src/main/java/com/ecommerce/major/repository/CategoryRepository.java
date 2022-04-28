cf70cf59187f3b56ab908e921fc03696ece4fb41
Ujjwal Gupta <52441813+ujjwal18bit1167@users.noreply.github.com>
2022-04-29 00:03:17 +0530

    Add files via upload

---
+++ src/main/java/com/ecommerce/major/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.ecommerce.major.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ecommerce.major.model.Category;
+
+public interface CategoryRepository extends JpaRepository<Category , Integer>{
+
+}
---
+++ src/main/java/com/ecommerce/major/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.ecommerce.major.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ecommerce.major.model.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Integer>{
+
+}
